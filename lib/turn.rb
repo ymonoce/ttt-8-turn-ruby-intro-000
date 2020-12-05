2930ea11c4b17b48ad4b20163d3e51b0dfbd19dc
Chloe Atwood <atwoodksw@gmail.com>
2020-12-05 03:56:19 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -8,3 +8,17 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} " # puts row 3 of board
 # end definition of method ln. six
 end
+
+# define method with parameters board array and index number on board array
+def valid_move? (board, index)
+
+# no position taken and index number between 0 and 8 returns true
+!position_taken?(board, index) && index.between?(0,8)
+end
+
+# define method with parameters board array and index number on board array
+def position_taken? (board, index)
+
+# index number on board array is equal to X or O this will return true, anything else will return false
+  board[index] == "X" || board[index] == "O"
+end
